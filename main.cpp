bd75b98a8ae60cf7ad0f83d65959c8ead92ffbca
Ian Coates <icoates@outlook.es>
2015-10-16 13:07:22 -0300

    Quedaron las imagenes en proporción 1/12 y centradas en las partes medias de la pantalla

--- main.cpp
+++ main.cpp
@@ -38,50 +38,66 @@ void MesaScene::init(){
 	m_pos.insert(std::pair<std::string, CIwFVec2>("maso", CIwFVec2(400, 10)));
 
 	temp = Iw2DCreateImage("reves2.png");
+	float prop = (float)Iw2DGetSurfaceWidth() / temp->GetWidth();
+	float fixpropx = temp->GetWidth()*prop / 12;  
+	float fixpropy = temp->GetHeight()*prop / 12; 
+	float xmedio = ((float)Iw2DGetSurfaceWidth() - fixpropx - 40) / 2;
+	float xderecha = ((float)Iw2DGetSurfaceWidth() - fixpropy - 10);
+	float ymedio = ((float)Iw2DGetSurfaceHeight() - fixpropx - 40) / 2;
+	float yabajo = ((float)Iw2DGetSurfaceHeight() - fixpropy - 10);
+	m_size.insert(std::pair<std::string, CIwFVec2>("reves", CIwFVec2(fixpropx, fixpropy)));
+	m_size.insert(std::pair<std::string, CIwFVec2>("reves90", CIwFVec2(fixpropy, fixpropx)));
+
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves1", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves1", CIwFVec2(180, 10)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves1", CIwFVec2(xmedio, 10)));
+	
+
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves2", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves2", CIwFVec2(200, 10)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves2", CIwFVec2(xmedio+20, 10)));
+	
+
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves3", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves3", CIwFVec2(220, 10)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves3", CIwFVec2(xmedio+40, 10)));
+	
 	
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves4", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves4", CIwFVec2(10, 100)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves4", CIwFVec2(10, ymedio)));
+	
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves5", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves5", CIwFVec2(10, 120)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves5", CIwFVec2(10, ymedio+20)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves6", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves6", CIwFVec2(10, 140)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves6", CIwFVec2(10, ymedio+40)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves7", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves7", CIwFVec2(180, 220)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves7", CIwFVec2(xmedio, yabajo)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves8", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves8", CIwFVec2(200, 220)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves8", CIwFVec2(xmedio+20, yabajo)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves9", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves9", CIwFVec2(220, 220)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves9", CIwFVec2(xmedio+40, yabajo)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves10", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves10", CIwFVec2(380, 100)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves10", CIwFVec2(xderecha, ymedio)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves11", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves11", CIwFVec2(380, 120)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves11", CIwFVec2(xderecha, ymedio+20)));
 
 	temp = Iw2DCreateImage("reves2.png");
 	m_imagenes.insert(std::pair<std::string, CIw2DImage *>("reves12", temp));
-	m_pos.insert(std::pair<std::string, CIwFVec2>("reves12", CIwFVec2(380, 140)));
+	m_pos.insert(std::pair<std::string, CIwFVec2>("reves12", CIwFVec2(xderecha, ymedio+40)));
 
 }
 
@@ -99,13 +115,21 @@ void MesaScene::Render(){
 	getBackGround()->Render();
 	for (std::map<std::string, CIw2DImage *>::iterator it = m_imagenes.begin(); it != m_imagenes.end(); ++it){
 		//IW_2D_IMAGE_TRANSFORM_ROT90 
-		if (it->first == "reves4" || it->first == "reves5" || it->first == "reves6" || it->first == "reves10" || it->first == "reves11" || it->first == "reves12"){
-			Iw2DSetImageTransform(IW_2D_IMAGE_TRANSFORM_ROT90);
+		
+
+		if (it->first == "maso")
+			Iw2DDrawImage(it->second, m_pos.find(it->first)->second); 
+		else{
+			if (it->first == "reves4" || it->first == "reves5" || it->first == "reves6" || it->first == "reves10" || it->first == "reves11" || it->first == "reves12"){
+				Iw2DSetImageTransform(IW_2D_IMAGE_TRANSFORM_ROT90);
+				Iw2DDrawImage(it->second, m_pos.find(it->first)->second, m_size.find("reves90")->second);
+			}
+			else {
+				Iw2DSetImageTransform(IW_2D_IMAGE_TRANSFORM_NONE);
+				Iw2DDrawImage(it->second, m_pos.find(it->first)->second, m_size.find("reves")->second);
+			}
+			
 		}
-		else {
-			Iw2DSetImageTransform(IW_2D_IMAGE_TRANSFORM_NONE);
-		}
-		Iw2DDrawImage(it->second, m_pos.find(it->first)->second); 
 	}
 }
 
--- main.h
+++ main.h
@@ -15,6 +15,7 @@ class MesaScene : public Scene
 private:
 	std::map<std::string, CIw2DImage*> m_imagenes;
 	std::map<std::string, CIwFVec2> m_pos;
+	std::map<std::string, CIwFVec2> m_size;
 public:
 		MesaScene();
 		~MesaScene();
